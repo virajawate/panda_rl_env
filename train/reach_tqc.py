a9e93eccc63c32c5e402706fe896502c14a91247
Viraj Awate <44661049+virajawate@users.noreply.github.com>
2026-09-11 00:03:23 +0530

    Editted:AddedTrainingScript

--- train/reach_tqc.py
+++ train/reach_tqc.py
@@ -18,6 +18,26 @@ def target_env(trail):
         train_time = 250_000
         env_opt = make_vec_env(env_id=env_id, n_envs=num_vec)
         env_norm = VecNormalize(env_opt)
+        dir = os.path.curdir
+        opt_log_dir = os.path.joint(dir, "logs", "optuna", f"{env_id}_{train_time}_{num_vec}")
+        tensor_log_dir = os.path.joint(dir, "logs", "tensor", f"TQC_{env_id}_{train_time}_{num_vec}")
+        model_params = sample_hypeparams(trail)
+        train_model = TQC(
+            **model_params,
+            policy="MultiInputPolicy",
+            env=env_norm,
+            replay_buffer_class=HerReplayBuffer,
+            tensorboard_log=tensor_log_dir,
+            verbose=0
+        )
+        train_model.learn(total_timesteps=train_time)
+        mean_reward, _ = evaluate_policy(
+            train_model,
+            env_norm,
+            n_eval_episodes=50,
+            render=True
+        )
+        return mean_reward
     except KeyboardInterrupt:
         print("UserInterupted")
         pass
